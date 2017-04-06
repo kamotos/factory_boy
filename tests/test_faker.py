a3e6942ca64a7a211c3f8f177c52775caf4f1c2b
Zahim Anass <zanass0@gmail.com>
2017-04-06 12:34:45 +0200

    Use the same random seed from fuzzy in faker. #closes 275
    - Functions such as `get_random_state` are both used by `factory.fuzzy` and `factory.faker`,
    it makes more sense to have them moved to a separate module : `random.py`
    - Adding depracation warnings for random state functions moved from `factory.fuzzy` to `factory.random`.
    - Updating changelog.

--- tests/test_faker.py
+++ tests/test_faker.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright: See the LICENSE file.
-
-
+import random
 import unittest
 
 import faker.providers
@@ -12,6 +11,7 @@ import factory
 class MockFaker(object):
     def __init__(self, expected):
         self.expected = expected
+        self.random = random.Random()
 
     def format(self, provider, **kwargs):
         return self.expected[provider]
